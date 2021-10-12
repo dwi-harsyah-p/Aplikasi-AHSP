46d58e6d3f7a3fa401ecd85682e62398cd9965c8
Ade Prastyo <biner.hexa99@gmail.com>
2021-10-12 10:10:29 +0700

    edit login

---
+++ application/controllers/User.php
@@ -0,0 +1,105 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class User extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+        if (!$this->session->userdata('nip')) {
+            redirect('auth');
+        }
+    }
+
+    public function index()
+    {
+        $data['judul'] = 'User Profile';
+        $data['user'] = $this->Ahsp_model->getTablewhere('biodata', 'nip', $this->session->userdata('nip'))->row_array();
+        $data['datauser'] = $this->Ahsp_model->getTablewhere('user', 'nip', $this->session->userdata('nip'))->row_array();
+
+        $this->form_validation->set_rules('nama', 'Nama', 'required|trim', ['required' => '{field} harus diisi']);
+        $this->form_validation->set_rules('gender', 'Jenis Kelamin', 'required|trim', ['required' => '{field} harus diisi']);
+        $this->form_validation->set_rules('ttl', 'Tanggal Lahir', 'required|trim', ['required' => '{field} harus diisi']);
+        $this->form_validation->set_rules('alamat', 'Alamat', 'required|trim', ['required' => '{field} harus diisi']);
+        $this->form_validation->set_rules('phone', 'Nomor Telepon', 'required|trim|numeric', [
+            'required' => '{field} harus diisi',
+            'numeric' => '{field} harus angka'
+        ]);
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('user/index', $data);
+            $this->load->view('templates/footer', $data);
+        } else {
+
+            //cek jikka ada gambar yang akan di upload
+            $upload_image = $_FILES['image']['name'];
+
+            if ($upload_image) {
+                $config['allowed_types'] = 'gif|jpg|png|jpeg';
+                // $config['max_size']      = '2048';
+                $config['upload_path'] = './assets/img/profile/';
+
+                $this->load->library('upload', $config);
+
+                if ($this->upload->do_upload('image')) {
+                    $old_image = $data['user']['image'];
+                    if ($old_image != 'default.jpg') {
+                        unlink(FCPATH . 'assets/img/profile/' . $old_image);
+                    }
+
+                    $new_image = $this->upload->data('file_name');
+                    $this->db->set('image', $new_image);
+                } else {
+                    echo $this->upload->display_errors();
+                }
+            }
+
+            $this->Ahsp_model->edit('biodata');
+            $this->session->set_flashdata('flash', 'Diubah');
+            redirect('user');
+        }
+    }
+
+    public function changepassword()
+    {
+        $data['judul'] = 'Ubah Password';
+        $data['user'] = $this->Ahsp_model->getTablewhere('biodata', 'nip', $this->session->userdata('nip'))->row_array();
+        $data['datauser'] = $this->Ahsp_model->getTablewhere('user', 'nip', $this->session->userdata('nip'))->row_array();
+
+        $this->form_validation->set_rules('password', 'Current password', 'required|trim', ['required' => '{field} harus diisi']);
+        $this->form_validation->set_rules('newpassword', 'New password', 'required|trim|min_length[3]', [
+            'required' => '{field} harus diisi',
+            'min_length' => '{field} minimal {param} karakter'
+        ]);
+        $this->form_validation->set_rules('confpass', 'Confirm new password', 'required|trim|matches[newpassword]', [
+            'required' => '{field} harus diisi',
+            'matches' => 'Konfirmasi password tidak sama'
+        ]);
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('user/changepassword', $data);
+            $this->load->view('templates/footer', $data);
+        } else {
+            $cur_pass = $this->input->post('password', true);
+            $new_pass = $this->input->post('newpassword', true);
+            if (!password_verify($cur_pass, $data['datauser']['password'])) {
+                $this->session->set_flashdata('msg', 'Password Salah');
+                redirect('user/changepassword');
+            } else {
+                if ($cur_pass == $new_pass) {
+                    $this->session->set_flashdata('msg', 'Password Tidak Boleh Sama dengan Yang Lama');
+                    redirect('user/changepassword');
+                } else {
+                    $pass = password_hash($new_pass, PASSWORD_DEFAULT);
+                    $this->Ahsp_model->changepassword($pass, $this->session->userdata('nip'));
+
+                    $this->session->set_flashdata('pass', 'Ubah Password');
+                    redirect('user/changepassword');
+                }
+            }
+        }
+    }
+}
---
+++ application/views/auth/login.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $judul; ?></title>
+
+    <link rel="stylesheet" href="<?= base_url('assets/'); ?>css/bootstrap.min.css">
+    <link href="<?= base_url('assets/'); ?>css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="container col-lg-5 col-md-7 col-sm-9 col-9">
+            <div class="row header p-lg-4 py-4">
+                <div class="header-login p-lg-2 p-0">
+                    <img src="<?= base_url('assets/'); ?>img/Logo_PU.jpg" class="logo me-3" width="20%">
+                    <h4 class="logo-text">BBWS SUMATERA VIII</h4>
+                    <!-- <h4 class="logo-text fs-6">DIREKTORAT JENDRAL SUMBER DAYA AIR</h4> -->
+                    <h4 class="logo-text">KEMENTERIAN PEKERJAAN UMUM DAN PERUMAHAN RAKYAT</h4>
+                </div>
+            </div>
+            <div class="row login-body">
+                <div class="card p-lg-4 p-4">
+                    <h2 class="text-center">Login</h2>
+                    <?= $this->session->flashdata('massage'); ?>
+                    <form class="p-lg-4" action="" method="post">
+                        <div class="mb-3">
+                            <!-- <label for="exampleInputEmail1" class="form-label">NIP</label> -->
+                            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="nip" placeholder="NIP/NRP">
+                            <small class="form-text text-danger"><?= form_error('nip'); ?></small>
+                        </div>
+                        <div class="mb-3">
+                            <!-- <label for="exampleInputPassword1" class="form-label">Password</label> -->
+                            <input type="password" class="form-control" id="exampleInputPassword1" name="password" placeholder="Password">
+                            <small class="form-text text-danger"><?= form_error('password'); ?></small>
+
+                            <p class="text-center fs-7">
+                                <a onclick="forget();" class="primary">Forgot password ? </a>
+                            </p>
+                        </div>
+                        <button type="submit" class="form-control btn btn-primary">LOGIN</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer d-flex justify-content-center align-items-end">
+            <p class="">Copyright&copy;BBWSSVIII2021</p>
+        </div>
+    </div>
+
+    <script src="<?= base_url('assets/'); ?>js/bootstrap.bundle.min.js"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script>
+        function forget() {
+            swal({
+                title: 'Forgot Password?',
+                text: 'Please Remember Your Password!',
+                icon: 'warning'
+            });
+        }
+    </script>
+</body>
+
+</html>
